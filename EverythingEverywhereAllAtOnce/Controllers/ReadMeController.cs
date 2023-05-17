0019669dd4184602f49515bb23c9836cf1f69ed1
R3ISilva <joaosilvaprivado@gmail.com>
2023-05-17 21:55:41 +0100

    initial commit

---
+++ EverythingEverywhereAllAtOnce/Controllers/ReadMeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using EverythingEverywhereAllAtOnce.Models;
+
+namespace EverythingEverywhereAllAtOnce.Controllers;
+
+public class ReadMeController : Controller
+{
+	public ReadMeController()
+	{
+        
+	}
+
+    public IActionResult Index()
+    {
+         return View();
+    }
+}
+
