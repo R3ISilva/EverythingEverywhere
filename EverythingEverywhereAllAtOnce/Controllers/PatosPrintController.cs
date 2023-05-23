db4425883e8b9a3549358ccc1a9bbe29ca3d020f
R3ISilva <joaosilvaprivado@gmail.com>
2023-05-23 22:10:16 +0100

    Update

---
+++ EverythingEverywhereAllAtOnce/Controllers/PatosPrintController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using EverythingEverywhereAllAtOnce.Models;
+
+namespace EverythingEverywhereAllAtOnce.Controllers;
+
+public class PatosPrintController : Controller
+{
+	public PatosPrintController()
+	{
+
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
+
