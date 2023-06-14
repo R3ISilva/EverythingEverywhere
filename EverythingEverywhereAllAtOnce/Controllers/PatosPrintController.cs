aadf94df2e64d86c42cfeeb902e9dba502057e3e
R3ISilva <joaosilvaprivado@gmail.com>
2023-06-15 00:21:00 +0100

    Converted html to cshtml, and added first base layer of connections (mvc)

--- EverythingEverywhereAllAtOnce/Controllers/PatosPrintController.cs
+++ EverythingEverywhereAllAtOnce/Controllers/PatosPrintController.cs
@@ -1,24 +1,71 @@
-﻿using System;
+﻿using EverythingEverywhereAllAtOnce.Models;
 using Microsoft.AspNetCore.Mvc;
-using EverythingEverywhereAllAtOnce.Models;
 
 namespace EverythingEverywhereAllAtOnce.Controllers;
 
 public class PatosPrintController : Controller
 {
-	public PatosPrintController()
-	{
-		
-	}
+    public PatosPrintController()
+    {
 
-	public IActionResult Index()
-	{
-		return View();
-	}
+    }
 
-	public IActionResult PrintTicket()
-	{
-		return View();
-	}
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult PrintTicket()
+    {
+        PrintTicketModel printTicketModel = new PrintTicketModel();
+        printTicketModel.FilamentList = GetFilamentList();
+        printTicketModel.UserList = GetUserList();
+
+        return View(printTicketModel);
+    }
+
+    /// <summary>
+    /// Gets all filament in stock
+    /// </summary>
+    /// <returns>List of all filament in stock</returns>
+    private List<Filament> GetFilamentList()
+    {
+        List<Filament> filamentList = new List<Filament>();
+
+        //TODO: GET FILLAMENT FROM DATABASE
+        filamentList.Add(new Filament { FilamentID = 1, FilamentName = "PLA PRETO" });
+        filamentList.Add(new Filament { FilamentID = 2, FilamentName = "PLA BRANCO" });
+        filamentList.Add(new Filament { FilamentID = 3, FilamentName = "PLA CINZENTO" });
+
+        return filamentList;
+    }
+
+    /// <summary>
+    /// Gets all users in database
+    /// </summary> 
+    /// <returns>List of all users in database</returns>
+    private List<User> GetUserList()
+    {
+        List<User> userList = new List<User>();
+
+        //TODO: GET USERS FROM DATABASE
+        userList.Add(new User { UserID = 1, UserName = "João" });
+        userList.Add(new User { UserID = 2, UserName = "Edgar" });
+        userList.Add(new User { UserID = 3, UserName = "Rato" });
+
+        return userList;
+    }
+
+    /// <summary>
+    /// Adds a new print ticket to database
+    /// </summary>
+    /// <param name="NewPrintTicket">Filled Model from view</param>
+    /// <returns>status and/or redirect</returns>
+    private IActionResult NewPrintTicket(PrintTicketModel NewPrintTicket)
+    {
+        return new JsonResult(true);
+        if (ModelState.IsValid)
+        { }
+    }  
 }
 
---
+++ EverythingEverywhereAllAtOnce/Models/PrintTicketModel.cs
@@ -0,0 +1,32 @@
+﻿namespace EverythingEverywhereAllAtOnce.Models;
+
+public class PrintTicketModel
+{
+    public int Id { get; set; }
+    public string? PrintName { get; set; }
+    public string? Weight { get; set; }
+    public List<Filament> FilamentList { get; set; }
+    public int FilamentID { get; set; }
+    public List<User> UserList { get; set; }
+    public int UserID { get; set; }
+    //missing:
+    //public image
+    //public file
+}
+
+public class Filament
+{
+    public int FilamentID { get; set; }
+    public string FilamentName { get; set; }
+    public int FilamentWeight { get; set; }
+
+}
+
+public class User
+{
+    public int UserID { get; set; }
+    public string UserName { get; set; }
+
+    //missing:
+    //public image
+}
